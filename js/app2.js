42e72755ba60a7729d95e77d0ea411ccb5f21fdc
Leonardo Pires <165698194+leonardojpires@users.noreply.github.com>
2025-06-06 12:19:14 +0100

    Changed layouts, created view to display students-teachers relations in the database

--- js/app2.js
+++ js/app2.js
@@ -2,9 +2,49 @@ let express = require('express');
 let app = express();
 let con = require('./db/connection.js');
 
+const path = require('path');
+
 /* Middleware para JSON */
 app.use(express.json());
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, '..', 'views'));
+
+
+/* ROTAS VIEWS */
+/* Definição das rotas */
+app.get('/', function(req, res) {
+    res.render('index', { title: 'ISTEC', message: 'Bem-Vindos' })
+});
+
+app.get('/view/alunos', function(req, res) {
+    con.query('SELECT * FROM students', function(err, result) {
+        if (err) throw err;
+        res.render('alunos', { title: 'Alunos', alunos: result })
+    })
+})
+
+app.get('/view/professores', function(req, res) {
+    con.query('SELECT * FROM teachers', function(err, result) {
+        if (err) throw err;
+        res.render('professores', { title: 'Professores', professores: result })
+    })
+})
+
+app.get("/view/alunos-orientadores", function(req, res) {
+    const sql = `SELECT students.name AS aluno, students.course AS curso, teachers.name AS orientador, teachers.email AS email FROM students LEFT JOIN teachers ON students.teacher_id = teachers.id`;
+
+    con.query(sql, function(err, result) {
+        if (err) return res.status(500).send(err);
+        res.render('alunos_professores', {
+            title: "Alunos e Orientadoress",
+            message: "Lista de Alunos com os seus Professores Orientadores",
+            alunos_professores: result,
+        });
+    });
+});
+
+/* ROTAS API */
 /* ---- ALUNOS ---- */
 app.get('/alunos', function(req, res) {
     con.query('SELECT * FROM students', function(err, result) {
